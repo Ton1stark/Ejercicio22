2948c21b3e61c99d7e0cc540f7b1b06378d02acd
Dell <Dell@DESKTOP-H0TE2P9>
2025-01-14 21:51:03 -0500

    carga inicial

---
+++ src/ejerciciop22/Empleados.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejerciciop22;
+
+/**
+ *
+ * @author Dell
+ */
+public class Empleados {
+    
+
+    static double calcular_salario_mensual(double salario_basico_hora, double numero_horas_trabajadas) {
+        double salario_mensual;
+     salario_mensual = salario_basico_hora * numero_horas_trabajadas;
+     
+             
+         if (salario_mensual > 450000){
+             return salario_mensual; 
+         }
+         else{
+             return salario_mensual; 
+         }
+    }
+    
+     public static String set_Nombre(String nombre){
+    
+            return nombre; 
+         
+     
+     }
+     
+     public static String limpiar_campos(){
+    
+            return ""; 
+         
+     
+     }
+   
+    
+ }
